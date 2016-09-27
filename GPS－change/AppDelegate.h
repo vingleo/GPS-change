361a33f17e17730ff669399eb6f7f8575298c0f0
vingleo <vingleo@vingleodemacbook-pro.local>
2016-09-27 23:29:54 +0800

    Initial Commit

---
+++ GPS－change/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPS－change
+//
+//  Created by vingleo on 16/9/27.
+//  Copyright © 2016年 vingleo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPS－change/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPS－change
+//
+//  Created by vingleo on 16/9/27.
+//  Copyright © 2016年 vingleo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
